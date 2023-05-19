7e40fb5d35a4ae400a863caf3c4920912b2bd234
Ali Movahedi <movahedi.4li@gmail.com>
2023-05-19 11:51:30 +0330

    Revert "chore: resolve comments"
    This reverts commit 9da311926e55ef0e93815f4d78174455f92f75cf.

--- ethereumetl/streaming/eth_streamer_adapter.py
+++ ethereumetl/streaming/eth_streamer_adapter.py
@@ -62,11 +62,10 @@ class EthStreamerAdapter:
             if i > 0:
                 if block['number'] - blocks[i - 1]['number'] != 1:
                     raise ValueError("out of order block list detected. block at index " + str(i) + " is " + str(block['number']) + ' and block at index ' + str(i - 1) + ' is ' + str(blocks[i-1]['number']))
-            else:
-                if i == 0:
-                    if blocks[0] - 1 != last_synced_block:
-                        raise ValueError("block number skipping detected. last sync block: " + last_synced_block + " target block: " + blocks[0])
 
+            if i == 0:
+                if blocks[0] - 1 != last_synced_block:
+                    raise ValueError("block number skipping detected. last sync block: " + last_synced_block + " target block: " + blocks[0])
 
 
         # Export receipts and logs
